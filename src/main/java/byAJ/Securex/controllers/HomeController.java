4c20dcd10c28887128fbbf74567b10b85815d907
AJ Henley <ajhenley@gmail.com>
2017-06-29 09:10:20 -0400

    sec messages advanced

--- src/main/java/byAJ/Securex/controllers/HomeController.java
+++ src/main/java/byAJ/Securex/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package byAJ.Securex.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request, Authentication authentication){
+        System.out.println(request.isUserInRole("ADMIN"));
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("User has authorities: " + userDetails.getAuthorities());
+
         return "index";
     }
 
